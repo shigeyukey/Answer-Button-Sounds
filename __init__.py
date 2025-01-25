85075526d6f53d80d398c9b7c1fd7efd7346ce9e
shigeyukey <shigge.3.21@gmail.com>
2025-01-25 15:24:16 +0900

    add license info

--- __init__.py
+++ __init__.py
@@ -1,6 +1,23 @@
-"""
-Answer Button Sounds addon for Anki 2.1.20 by Kyle "Khonkhortisan" Mills
-"""
+# Answer Button Sounds addon
+# Copyright (C) 2019-2020 Kyle "Khonkhortisan" Mills <https://github.com/khonkhortisan>
+# Answer Button Sounds (original)
+	# Ankiweb https://ankiweb.net/shared/info/679615590
+	# github https://github.com/khonkhortisan/Answer-Button-Sounds
+
+# Copyright (C) 2025 Shigeyuki <http://patreon.com/Shigeyuki>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import os
 
